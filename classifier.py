0536ba2edb51e13eef023b9e476b2d3c15d5134d
shir308 <shirshir1995>
2021-06-03 10:12:12 +0300

    preprocessing

--- classifier.py
+++ classifier.py
@@ -11,16 +11,14 @@ def load_data(path):
     """
     data = pd.read_csv(path)
     data = data.drop(["IUCR", "FBI Code", "Description"], axis=1)
-    data[["new_date"]] = data["Date"].str.split(" ", expand = True)[0]
-    data[["Time"]] = data["Date"].str.split(" ", expand = True)[1]
-    data[["Am_Pm"]] = data["Date"].str.split(" ", expand = True)[2]
-
+    data[["new_date", "Time"]] = data["Date"].str.split(" ", 1, expand = True)
     data["new_date"] = pd.to_datetime(data["new_date"], dayfirst=True)
     data["day_of_week"] = data["new_date"].dt.dayofweek
     data[["day", "month", "year_and_time"]] = data["Date"].str.split("/", expand = True)
-    data[["Time"]] = data["year_and_time"].str.split(" ", expand= True)[1]
+    data["Time"] = pd.to_datetime(data["Time"]).dt.time
+
     data = data.drop("new_date", axis=1)
-    data = data.drop("new_date", axis=1)
+    data = data.drop("year_and_time", axis=1)
 
     # data = data.drop("year_and_time")
     # x_train = data.sample(frac=0.43)
@@ -41,4 +39,4 @@ def send_police_cars(X):
     pass
 
 
-load_data("Dataset_crimes.csv")
+load_data("train_data.csv")
